23607e4f84c5e825c40f44fd44b58e07e0aa95a3
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-07-26 22:06:32 +0900

    [level 2] Title: 타겟 넘버, Time: 7.64 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    static int[] numbers;
+    static int target;
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers.clone();
+        this.target = target;
+        
+        DFS(0, 0);
+        return answer;
+    }
+    
+    public void DFS (int d, int sum) {
+        if (d == numbers.length) {
+            if (sum == target) answer++;
+        }
+        if (d < numbers.length) {
+            DFS(d+1, sum+numbers[d]);
+            DFS(d+1, sum-numbers[d]);
+        }
+    }
+}
