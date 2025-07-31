6322974e21472639a93c082f34ccc782722ff476
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-07-31 18:49:41 +0900

    [level 3] Title: 섬 연결하기, Time: 1.46 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
+        Edge[] edges = new Edge[costs.length];
+        for (int i = 0; i< edges.length; i++) {
+            edges[i] = new Edge(costs[i][0], costs[i][1], costs[i][2]);
+        }
+        
+        Node[] nodes = new Node[n];
+        for (int i = 0; i<n; i++) {
+            nodes[i] = new Node();
+        }
+        
+        int totalCost = 0;
+        
+        for (Edge edge : edges) {
+            Node node1 = nodes[edge.u];
+            Node node2 = nodes[edge.v];
+            
+            if (node1.isConnected(node2)) continue;
+            node1.merge(node2);
+            totalCost += edge.cost;
+        }
+        
+        return totalCost;
+    }
+    
+    private static class Edge {
+        public final int u;
+        public final int v;
+        public final int cost;
+        
+        public Edge(int u, int v, int cost) {
+            this.u = u;
+            this.v = v;
+            this.cost = cost;
+        }
+    }
+    
+    private static class Node {
+        private Node parent = null;
+        
+        public boolean isConnected(Node node) {
+            return root() == node.root();
+        }
+        
+        public void merge(Node node) {
+            if (isConnected(node)) return;
+            node.root().parent = this.root();
+        }
+        
+        private Node root() {
+            if (parent == null) return this;
+            else {
+                return parent = parent.root();
+            }
+        }
+    }
+}
