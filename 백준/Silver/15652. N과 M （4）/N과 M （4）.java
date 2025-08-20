79643797cb4169451b8c50d42455e178cab0a9a7
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-08-20 15:38:24 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16832 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(new BufferedReader(new InputStreamReader(System.in)).readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        dfs(0, 1);
+        System.out.println(sb.toString().trim());
+    }
+
+    public static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int d : arr) {
+                sb.append(d).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        else {
+            for (int i = start; i<=n; i++) {
+                arr[depth] = i;
+                dfs(depth+ 1, i);
+            }
+        }
+    }
+}
