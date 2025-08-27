410183cd8e3c8dae79391ff2853db5f4cec5aebc
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-08-27 14:13:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 53364 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,32 +1,44 @@
+import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int k = kb.nextInt();
-        int[][] items = new int[n][2]; // 물건들의 {무게, 가치} 가 들어있는 2차원 배열 
-        for (int i = 0; i<n; i++) {
-            items[i][0] = kb.nextInt();
-            items[i][1] = kb.nextInt();
+    static int[] v;
+    static int[] w;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        v = new int[n];
+        w = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
-        
-        int[][] dpTable = new int[n+1][k+1];
-        
-        for (int x = 1; x<n+1; x++) {
-            for (int y = 1; y<k+1; y++) {
-                int w = items[x - 1][0];
-                int v = items[x - 1][1];
-                
-                int up = dpTable[x-1][y]; // 선택된 물건을 포함하지 않는 경우
-                int cross = 0;
-                if (y - w >= 0) {
-                    cross = dpTable[x - 1][y - w] + v;
-                }
-                
-                dpTable[x][y] = Math.max(up, cross);
+
+
+        dp = new int[n][k + 1];
+
+        for (int j = 0; j <= k; j++) {
+            if (w[0] <= j) {
+                dp[0][j] = v[0];
             }
         }
-        
-        System.out.println(dpTable[n][k]);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (w[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]);
+                }
+            }
+        }
+
+        System.out.println(dp[dp.length - 1][k]);
     }
 }
