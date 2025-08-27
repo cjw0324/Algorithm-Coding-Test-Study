38077011b4681b35c00d209df938e75166a79a60
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-08-27 14:49:02 +0900

    [Silver III] Title: 수열, Time: 256 ms, Memory: 23808 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int max = sum;
+        for (int i = 0; i < n - k; i++) {
+            int temp = sum - arr[i] + arr[i + k];
+            max = Math.max(max, temp);
+            sum = temp;
+        }
+
+        System.out.println(max);
+    }
+}
