4ff161f09d17a67c5aa25b9ad0c7265343dea38b
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-09-19 18:26:27 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26304 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> n1 - n2);
+
+        StringBuilder sb = new StringBuilder();
+
+        int testcase = 0;
+        while (testcase < n) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(input);
+            }
+            testcase++;
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+
+}
