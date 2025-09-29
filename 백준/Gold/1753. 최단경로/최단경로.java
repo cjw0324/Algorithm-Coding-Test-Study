da32200c3b7c83fac566c84b53f0fab7c69d9ff8
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-09-29 15:36:28 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 108152 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,72 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static class Node {
-        int end;
-        int weight;
-
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
-        }
-    }
-
-    static int v, e, k;
-    static boolean[] visited;
-    static List<Node>[] list;
-    static int[] dpTable;
+    static int v, e;
+    static int k;
+    static List<Node>[] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(br.readLine());
-
-        list = new List[v + 1];
-        visited = new boolean[v + 1];
-        dpTable = new int[v + 1];
-
+        graph = new List[v + 1];
         for (int i = 0; i <= v; i++) {
-            list[i] = new ArrayList<>();
-            dpTable[i] = Integer.MAX_VALUE;
+            graph[i] = new ArrayList<>();
         }
-
-        for (int i = 1; i <= e; i++) {
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            list[u].add(new Node(v, w));
+            graph[u].add(new Node(v, w));
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.weight - n2.weight);
-        dpTable[k] = 0;
-        pq.offer(new Node(k, 0));
+
+        Dijkstra(k);
+    }
+
+    static void Dijkstra(int start) {
+        boolean[] visited = new boolean[v + 1];
+        int[] dist = new int[v + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start]  = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.cost - n2.cost);
+        pq.offer(new Node(start, 0));
 
         while(!pq.isEmpty()) {
-            Node now = pq.poll();
-            if (!visited[now.end]) visited[now.end] = true;
+            int now = pq.poll().index;
+            if (!visited[now]) {
+                visited[now] = true;
 
-            for (int i = 0; i < list[now.end].size(); i++) {
-                Node next = list[now.end].get(i);
-
-                if (!visited[next.end] && now.weight + next.weight < dpTable[next.end]) {
-                    dpTable[next.end] = now.weight + next.weight;
-                    pq.offer(new Node(next.end, dpTable[next.end]));
+                for (Node next : graph[now]) {
+                    if (dist[next.index] > dist[now] + next.cost) {
+                        dist[next.index] = dist[now] + next.cost;
+                        pq.offer(new Node(next.index, dist[next.index]));
+                    }
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= v; i++) {
-            if (dpTable[i] == Integer.MAX_VALUE) {
-                sb.append("INF").append("\n");
+        for (int i = 1; i<=v; i++) {
+            int d = dist[i];
+            if (d == Integer.MAX_VALUE) {
+                sb.append("INF\n");
             } else {
-                sb.append(dpTable[i]).append("\n");
+                sb.append(d).append("\n");
             }
         }
 
         System.out.println(sb.toString().trim());
+
+    }
+
+    static class Node {
+        int index;
+        int cost;
+
+        public Node(int i, int c) {
+            this.index = i;
+            this.cost = c;
+        }
     }
 }
