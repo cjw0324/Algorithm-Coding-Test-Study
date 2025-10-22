7ab9370057b2f857e27ece6b810560c7ba51be26
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-10-22 13:21:18 +0900

    [Bronze IV] Title: 세 수, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> n2 - n1);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 3; i++) {
+            pq.offer(Integer.parseInt(st.nextToken()));
+        }
+
+        pq.poll();
+        System.out.println(pq.poll());
+    }
+}
