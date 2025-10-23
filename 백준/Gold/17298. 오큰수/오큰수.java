79e6d7d2d6d9a4099d66dad7c0797c105ae39dbb
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-10-23 09:18:33 +0900

    [Gold IV] Title: 오큰수, Time: 880 ms, Memory: 151856 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
