6db81bd2142365b9912f13cd69b51c87c7cb83e6
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-10-25 01:37:30 +0900

    [Silver V] Title: 귀찮음, Time: 332 ms, Memory: 49188 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.java
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -3,32 +3,25 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] arr;
-    static int answer = 0;
+    static long[] arr;
+    static long answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        arr = new long[n];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long length = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            length += arr[i];
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            long temp = arr[i];
+            length -= temp;
+            answer += temp * length;
+        }
 
-        calc(0, n - 1);
         System.out.println(answer);
-    }
-
-    static int calc(int start, int end) {
-        if (start == end) {
-            return arr[start];
-        }
-        int mid = (start + end) / 2;
-        int head = calc(start, mid);
-        int tail = calc(mid + 1, end);
-        answer += (head * tail);
-        return head + tail;
-
     }
 }
