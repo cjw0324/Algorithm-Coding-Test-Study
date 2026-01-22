e4523fa4b009a575e7fd7c958ef94d03cae5722f
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2026-01-22 14:19:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 848 ms, Memory: 92136 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * N 개 정점, M 개 간선 무방향 그래프
+ * 모든 간선 가중치 1
+ * 정점 R 시작하여 BFS 탐색 시 방문 순서 출력
+ * 방문 순서는 정점 번호의 오름차순
+ */
+
+public class Main {
+    static StringTokenizer st;
+    static int N, M, R;
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int score = 1;
+    static int[] order;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int n = 0; n <= N; n++) {
+            graph[n] = new ArrayList<>();
+        }
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int n = 0; n <= N; n++) {
+            Collections.sort(graph[n], Collections.reverseOrder());
+        }
+
+
+        order = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        bfs();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(order[i]).append("\n");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+
+    static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(R);
+        visited[R] = true;
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            order[now] = score++;
+            for (int next : graph[now]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+
+    }
+}
