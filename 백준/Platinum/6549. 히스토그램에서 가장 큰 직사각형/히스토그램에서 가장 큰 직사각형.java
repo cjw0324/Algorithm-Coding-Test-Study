361f703edb4447af15d84920d769cf457aaef337
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2026-03-09 22:45:41 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 492 ms, Memory: 45808 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -3,72 +3,66 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] h;
+    static int[] histogram;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        while (true) {
-            st = new StringTokenizer(br.readLine());
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             n = Integer.parseInt(st.nextToken());
             if (n == 0) break;
-            h = new int[n];
+
+            histogram = new int[n];
             for (int i = 0; i < n; i++) {
-                h[i] = Integer.parseInt(st.nextToken());
+                histogram[i] = Integer.parseInt(st.nextToken());
             }
 
-            long max = getSub(0, n - 1);
-            sb.append(max).append("\n");
+            sb.append(getMaxArea(0,n-1)).append("\n");
         }
         System.out.println(sb.toString().trim());
     }
 
-    private static long getSub(int lt, int rt) {
-        if (lt == rt) {
-            return h[lt];
-        }
-        int mid = (lt + rt) / 2;
-        long left = getSub(lt, mid);
-        long right = getSub(mid + 1, rt);
-
-        long max = Math.max(left, right);
-
-        max = Math.max(max, getMid(lt, rt, mid));
-
-        return max;
-    }
-
-    private static long getMid(int leftEnd, int rightEnd, int mid) {
-        int left = mid;
-        int right = mid;
-
-        long height = h[mid];
-        long maxArea = height;
-
-        while (leftEnd < left && right < rightEnd) {
-            if (h[left - 1] > h[right + 1]) {
-                left--;
-                height = Math.min(height, h[left]);
-            } else {
-                right++;
-                height = Math.min(height, h[right]);
-            }
-
-            maxArea = Math.max(maxArea, (right - left + 1) * height);
+    private static long getMaxArea(int left, int right) {
+        if (left == right) {
+            return histogram[left];
         }
 
-        while (right < rightEnd) {
-            right++;
-            height = Math.min(height, h[right]);
-            maxArea = Math.max(maxArea, (right - left + 1) * height);
-        }
+        int mid = (left + right) / 2;
+        long maxArea = Math.max(getMaxArea(left, mid), getMaxArea(mid + 1, right));
 
-        while (left > leftEnd) {
-            left--;
-            height = Math.min(height, h[left]);
-            maxArea = Math.max(maxArea, (right - left + 1) * height);
-        }
+        maxArea = Math.max(maxArea, getMidArea(left, mid, right));
 
         return maxArea;
+    }
+
+    private static long getMidArea(int maxLeft, int mid, int maxRight) {
+        long height = histogram[mid];
+        long area = height;
+        int moveLeft = mid, moveRight = mid;
+
+        while (maxLeft < moveLeft && moveRight < maxRight) {
+            if (histogram[moveLeft - 1] < histogram[moveRight + 1]) {
+                moveRight++;
+                height = Math.min(height, histogram[moveRight]);
+            } else {
+                moveLeft--;
+                height = Math.min(height, histogram[moveLeft]);
+            }
+            area = Math.max(area, (moveRight - moveLeft + 1) * height);
+        }
+
+        while (maxLeft < moveLeft) {
+            moveLeft--;
+            height = Math.min(height, histogram[moveLeft]);
+            area = Math.max(area, (moveRight - moveLeft + 1) * height);
+        }
+
+        while (moveRight < maxRight) {
+            moveRight++;
+            height = Math.min(height, histogram[moveRight]);
+            area = Math.max(area, (moveRight - moveLeft + 1) * height);
+        }
+
+        return area;
     }
 }
