8679853b08ad50d969ff845c3aeff8b6137e4ad9
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2026-03-13 15:49:50 +0900

    [Gold III] Title: 파일 합치기, Time: 488 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,37 +1,34 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
-            int K = Integer.parseInt(br.readLine());
-            int[] nums = new int[K + 1];
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 0; testCase < T; testCase++) {
+            int k = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int i = 1; i <= K; i++) {
-                nums[i] = Integer.parseInt(st.nextToken());
+            int[] sum = new int[k+1];
+            int[][] dp = new int[k+1][k+1];
+
+            for (int i = 1; i <= k; i++) {
+                sum[i] = Integer.parseInt(st.nextToken()) + sum[i - 1];
             }
-            int[] sums = new int[K + 1];
-            for (int i = 1; i <= K; i++) {
-                sums[i] = sums[i - 1] + nums[i];
-            }
-            int[][] dp = new int[K + 1][K + 1];
-            for (int i = 1; i <= K; i++) {
-                for (int from = 1; from + i <= K; from++) {
-                    int to = from + i;
-                    int min = Integer.MAX_VALUE;
-                    for (int divide = from; divide < to; divide++) {
-                        min = Math.min(min, dp[from][divide] + dp[divide + 1][to]);
+
+            for (int length = 2; length <= k; length++) {
+                for (int start = 1; start <= k - length + 1; start++) {
+                    int end = start + length - 1;
+                    dp[start][end] = Integer.MAX_VALUE;
+                    for (int mid = start; mid < end; mid++) {
+                        dp[start][end] = Math.min(dp[start][end],
+                                dp[start][mid] + dp[mid + 1][end] + sum[end] - sum[start - 1]);
                     }
-                    dp[from][to] = min + sums[to] - sums[from - 1];
                 }
             }
-            sb.append(dp[1][K]+"\n");
+            sb.append(dp[1][k]).append("\n");
         }
-
         System.out.println(sb.toString().trim());
     }
 }
