ec8469a445cfc1b151f8d58f91f5cf5248757b5c
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2026-03-13 16:29:01 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,28 +1,39 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     static int n;
+    static int[][] matrix;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int[][] matrix = new int[n][2];
-        int[][] dp = new int[n][n];
+        matrix = new int[n][2];
+        dp = new int[n][n];
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine(), " ");
             matrix[i][0] = Integer.parseInt(st.nextToken());
             matrix[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int from = 0; from + i < n; from++) {
-                int to = from + i;
-                dp[from][to] = Integer.MAX_VALUE;
-                for (int divide = from; divide < to; divide++) {
-                    dp[from][to] = Math.min(dp[from][to], dp[from][divide] + dp[divide + 1][to] + matrix[from][0] * matrix[divide][1] * matrix[to][1]);
+
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+        for (int gap = 1; gap < n; gap++) {
+            for (int start = 0; start + gap < n; start++) {
+                int end = start + gap;
+                dp[start][end] = Integer.MAX_VALUE;
+                for (int mid = start; mid < end; mid++) {
+                    dp[start][end] = Math.min(dp[start][end],
+                            dp[start][mid] + dp[mid + 1][end] + matrix[start][0] * matrix[mid][1] * matrix[end][1]);
                 }
             }
         }
-        System.out.println(dp[0][n - 1]);
+
+        System.out.println(dp[0][n-1]);
     }
 }
