06e944ff56c9035dcfb1fa27e024445673629678
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2026-04-04 00:39:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.36 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,39 @@
 import java.util.*;
 class Solution {
-    static final int[] dx = {0,0,-1,1}; //상하좌우 이동에 대한 x 변화량
-    static final int[] dy = {1,-1,0,0}; //상하좌우 이동에 대한 y 변화량
-    static int[][] maps;
-    static int[][] visited;
     static int n,m;
+    static final int[] dx = {0,0,1,-1};
+    static final int[] dy = {1,-1,0,0};
+    static int[][] distance;
     public int solution(int[][] maps) {
-        this.maps = maps.clone();
         n = maps.length;
         m = maps[0].length;
-        visited = new int[n][m];
-        
-        BFS(new Point(0,0));
-        
-        return (visited[n-1][m-1] != 0) ? visited[n-1][m-1] : -1;  
+        distance = new int[n][m];
+        return bfs(new Point(0,0), maps);
     }
     
-    public void BFS(Point p) {
+    static private int bfs(Point p, int[][] maps) {
         Queue<Point> queue = new LinkedList<>();
-        visited[p.x][p.y] = 1;    // 출발위치의 이동 거리는 0으로 설정
-        queue.offer(p);           // 큐에 넣는다
-        
+        distance[p.x][p.y] = 1;
+        queue.offer(p);
         while(!queue.isEmpty()) {
             Point now = queue.poll();
             for (int i = 0; i<4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && visited[nx][ny] == 0) {
-                    Point next = new Point(nx, ny);
-                    visited[next.x][next.y] = visited[now.x][now.y] + 1;
-                    queue.offer(next);
+                int next_x = now.x + dx[i];
+                int next_y = now.y + dy[i];
+                if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < m && distance[next_x][next_y] == 0 && maps[next_x][next_y] == 1) {
+                    queue.offer(new Point(next_x, next_y));
+                    distance[next_x][next_y] = distance[now.x][now.y] + 1;
+                    if (next_x == n-1 && next_y == m-1) return distance[n-1][m-1];
                 }
             }
         }
+        return -1;
     }
     
-    
-    class Point {
+    static class Point {
         int x;
         int y;
+        
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
